76e5796a49cd24dadcab3b04e3ca192b1e8bb057
Victor Wolff <victorwolff39@gmail.com>
2020-09-19 01:25:02 -0300

    Added function to delete products from cart using a dismissible (swipe to delete)

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,27 +9,52 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        vertical: 4,
-        horizontal: 15,
+    return Dismissible(
+      /*
+       * Dismissible properties:
+       */
+      key: ValueKey(cartItem.product.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 30),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text(
-                  '${cartItem.price}'
+      onDismissed: (_) {
+        Provider.of<Cart>(context, listen: false).removeItem(cartItem.product.id);
+      },
+      /*
+       * End of dismissible properties...
+       */
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          vertical: 4,
+          horizontal: 15,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('${cartItem.price}'),
                 ),
               ),
             ),
+            title: Text(cartItem.product.title),
+            subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+            trailing: Text('${cartItem.quantity}x'),
           ),
-          title: Text(cartItem.product.title),
-          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
-          trailing: Text('${cartItem.quantity}x'),
         ),
       ),
     );
