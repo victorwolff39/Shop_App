857dc6688e73c52506ff1a27884086a0400459c5
Victor Wolff <victorwolff39@gmail.com>
2021-01-20 12:38:44 -0300

    Update endpoints.dart

--- lib/utils/endpoints.dart
+++ lib/utils/endpoints.dart
@@ -1,10 +1,10 @@
 class Endpoints {
   static const REALTIME_DATABASE =
-      'https://flutter-testapp-49112.firebaseio.com/';
-  static const AUTH_SIGNUP_API = 'https://identitytoolkit.googleapis.com/v1/'
-      'accounts:signUp?key=AIzaSyCG2yXsAyUwanTK1huLBeRnH889k7etKu4';
-  static const AUTH_SIGNIN_API = 'https://identitytoolkit.googleapis.com/v1/'
-      'accounts:signInWithPassword?key=AIzaSyCG2yXsAyUwanTK1huLBeRnH889k7etKu4';
+      '';
+  static const AUTH_SIGNUP_API = ''
+      '';
+  static const AUTH_SIGNIN_API = ''
+      '';
 
   static const USER_FAVORITES = '${REALTIME_DATABASE}userFavorites';
   static const PRODUCTS = '${REALTIME_DATABASE}products';
